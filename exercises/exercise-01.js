04b9b113a755d61fdb41a031bd434b82077d0be1
Leigha De La Rosa <cdversiontwo@gmail.com>
2022-12-05 18:14:43 -0800

    partial answer to first problem

--- exercises/exercise-01.js
+++ exercises/exercise-01.js
@@ -1,2 +1,13 @@
 // Use the months array from index.js.
 // Write a function that returns only the months ending in the letter 'y'.
+const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function monthsEndingInY(arr) {
+    arr.map(element => {
+        if (element[length -1] = 'y') {
+            element 
+        }
+    });
+}
+
+monthsEndingInY(monthsArray);
