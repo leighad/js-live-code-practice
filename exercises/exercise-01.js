ebbb7d7d5add349a1f555a6b48471e1c268b4e18
Leigha De La Rosa <cdversiontwo@gmail.com>
2023-02-14 21:03:13 -0800

    first attempt first exercise

--- exercises/exercise-01.js
+++ exercises/exercise-01.js
@@ -3,11 +3,27 @@
 const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 function monthsEndingInY(arr) {
-    arr.map(element => {
-        if (element[length -1] = 'y') {
-            element 
-        }
+    yMonthsArr = [];
+    arr.forEach(element => {
+        let splitEl = element.split(',')
+        console.log(splitEl)
+        // console.log(toChars)
+        // let toChars = splitEl.split(',')
+        splitEl.forEach(el => {
+         let toChars = el.split(',')
+            // element.forEach(el => {
+            console.log(toChars)
+            // if (el.last == 'y') {
+            //     console.log(element)
+            // } else {
+            //     console.log("Nope!!")
+            // }
+        })
+        // if ((splitEl.at(-1)) == 'y') {
+        //     yMonthsArr.push(element) 
+        // }
     });
+    // return yMonthsArr;
 }
 
 monthsEndingInY(monthsArray);
